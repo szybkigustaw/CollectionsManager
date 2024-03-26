2f026e2bcf90d4837cae174ffa4633aead438e3c
Michał Mikuła <mikulamichal@proton.me>
2024-03-26 12:50:51 +0100

    First commit

---
+++ CollectionsManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionsManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CollectionsManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionsManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
